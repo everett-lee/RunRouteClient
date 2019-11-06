bc95cf1f3e3a120297d3ce4e59e08828b18e2c57
everett-lee <everett.lee@btinternet.com>
2019-11-06 22:50:39 +0000

    tests reflect use of hooks

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,9 +2,13 @@ import React from 'react';
 import App from '../components/App.js';
 import { render, fireEvent, cleanup } from '@testing-library/react';
 
-import routePromise from '../__mocks__/routePromise';
-
 afterEach(cleanup)
+
+// mock the map
+jest.mock('../components/map/MapDisplay', () => {
+    const MapDisplay = () => <div />;
+    return MapDisplay;
+})
 
 // mock the coord provider from the coordinates context 
 jest.mock('../components/providers/CoordProvider', () => {
@@ -14,14 +18,14 @@ jest.mock('../components/providers/CoordProvider', () => {
 // mock the api provided method from the api context
 jest.mock('../components/providers/ApiProvider', () => {
     const route = {
-        routeCoords: {lat: 11, lon: 50},
+        routeCoords: [[51,50], [50, 49], [49, 48]],
         routeName: "Test route",
-        routeDistance: 5,
+        routeDistance: 5000,
         routeGradient: -11 };
 
     return { ApiContext: { sendRequest: (a,b,c) => {
         return new Promise (
-            (resolve, reject) => resolve(route));
+            (resolve, reject) => resolve? resolve(route): reject(new Error()));
     }} }
 })
 
@@ -30,7 +34,7 @@ jest.mock('react', () => {
     const RealReact = require.requireActual('react')
     return {
         ...RealReact,
-        useContext: arg => arg,
+        useContext: arg => arg
     }
 })
 
@@ -51,11 +55,32 @@ test('Successful display of start coords', () => {
     expect(res.textContent).toContain(`(${lat}, ${lon})`)
 });
 
-test('Displays updated route details when api reponse recieved', () => {
-    const { queryByText, getByText } = render(<App />);
-    
+test('Displays updated route details when api reponse recieved', async () => {
+    const { getByText } = render(<App />);
+
     fireEvent.click(getByText("Find route"))
 
-    // // the modal appears in the DOM after sign up is clicked 
-    // expect(getByText(/register/i).textContent).toBe('Register');
+    // the distance is updated after successful api call
+    const distanceDisplay = await getByText(/Distance/);
+    expect(distanceDisplay.textContent).toContain(5)
+
+    // the gradient is upadated after successful api call
+    const gradientDisplay = await getByText(/Average gradient/);
+    expect(gradientDisplay.textContent).toContain(-11)
+});
+
+test('Stores route to the sidebar when saved', async () => {
+    const { getByText } = render(<App />);
+
+    fireEvent.click(getByText("Find route"))
+
+    const distanceDisplay = await getByText(/Distance/);
+    expect(distanceDisplay.textContent).toContain(5)
+
+    fireEvent.click(getByText("Save route"))
+
+    // the sidebar is updated with the displayed route 
+    // on saving 
+    const sideBarDisplay = await getByText(/Test route/);
+    expect(sideBarDisplay.textContent).toContain("Test")
 });
